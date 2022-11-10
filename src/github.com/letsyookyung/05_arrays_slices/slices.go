a8a2de792e85f95ad048b5d9f67c00582b54e22a
ivy_git <yookyung.lee@outlook.kr>
2022-11-10 09:34:25 +0900

    5일차 arrays 와 slices

---
+++ src/github.com/letsyookyung/05_arrays_slices/slices.go
@@ -0,0 +1,12 @@
+package main
+
+//func main() {
+//	a := []int{1, 2, 3}
+//	//b := &a // slices로 하면, &안써도 call bu reference가 됨
+//	b := a
+//	b[1] = 5
+//	fmt.Println(a)
+//	fmt.Println(b)
+//	fmt.Printf("len: %v\n", len(a))
+//	fmt.Printf("capacity:%v\n", cap(a))
+//}
